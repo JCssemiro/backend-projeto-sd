452c5540f06b97808a1a5d4b4d1435126fa5eac0
Jcssemiro <juancssemiro@gmail.com>
2025-07-10 19:23:07 -0400

    first commit

---
+++ src/main/java/com/juancassemiro/backendprojetosd/BackEndProjetoSdApplication.java
@@ -0,0 +1,13 @@
+package com.juancassemiro.backendprojetosd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProjetoSdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndProjetoSdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juancassemiro/backendprojetosd/BackEndProjetoSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancassemiro.backendprojetosd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProjetoSdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
